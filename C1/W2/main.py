a0f4332c9af30761469806b12d650c11dcff8296
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-10-11 22:16:40 +0700

    Neural Network Definition of 3 Layers

--- C1/W2/main.py
+++ C1/W2/main.py
@@ -6,3 +6,11 @@ from tensorflow import keras
 fashion_mnist = tf.keras.datasets.mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# Neural Network Definition of 3 Layers
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
