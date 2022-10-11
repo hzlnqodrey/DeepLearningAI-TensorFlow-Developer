d51f608d5430ff923d172b0fe876a5ffd9d5c3a1
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-10-11 22:19:26 +0700

    first layer is flatten layer

--- C1/W2/main.py
+++ C1/W2/main.py
@@ -10,6 +10,8 @@ fashion_mnist = tf.keras.datasets.mnist
 # Neural Network Definition of 3 Layers
 
 model = keras.Sequential([
+    # So the first layer is a flatten layer with input shaping 28 by 28 | because we specify that input data (mnist data) is 28x28 array bit | we should expect the data to be in
+    # the flatten takes this 28 by 28 square and turn it into a simple linear array
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
